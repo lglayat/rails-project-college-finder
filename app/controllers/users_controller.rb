c67105636ee2485581321c4b73f38366cc0a24b7
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-23 10:43:09 -0400

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
@@ -15,9 +21,17 @@ class UsersController < ApplicationController
 		end
 	end
 
-	def show
+	def edit 
 		@user = User.find(params[:id])
 	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update(user_params)
+
+		redirect_to user_path(@user)
+	end
+
 
 
 	private
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,7 @@ require 'csv'
 require 'pry'
 # require_all 'app/models'
 
-# CSV.foreach('db/data.csv') do |row|
-# 	college = College.new(name: row[3], state: row[5], sat_score: row[60], act_avg: row[56], cost: row[376] )
-# 	binding.pry
-# end
+
 
 
 csv_text = File.read('db/data.csv')
@@ -14,17 +11,6 @@ csv.each do |row|
 	college = College.create(name: row[0], state: row[2], sat_score: row[6], act_avg: row[7], cost: row[46] )
 end
 
-#associations
-# field_lengths = {}
-
-# CSV.foreach('db/data.csv', :headers => true, :header_converters => :symbol) do |csv_row|
-# 	csv_row.each do |k, v|
-# 		field_lengths[k] = [field_lengths[k] || 0, v.length].max
-# 	end
-# 	binding.pry
-# 	college = College.create(name: field_lengths{:INSTM}, state: row[2], sat_score: row[6], act_avg: row[7], cost: row[46] )
-# 	field_lengths = {}
-# end
 
 
 programs_array = ["English Language and Literature", "Education", "Architecture", "Agriculture", 
@@ -34,4 +20,18 @@ programs_array = ["English Language and Literature", "Education", "Architecture"
 	"Visual and Performing Arts", "Business", "History"]
 programs_array.each do |program|
 	program = Program.create(name: program)
+end
+
+
+#associations
+field_lengths = {}
+
+CSV.foreach('db/data.csv', :headers => true, :header_converters => :symbol) do |csv_row|
+	csv_row.each do |k, v|
+		field_lengths[k] = [field_lengths[k] || 0, v.length].max
+	end
+	binding.pry
+	# college = College.new(name: field_lengths{:INSTM}, state: row[2], sat_score: row[6], act_avg: row[7], cost: row[46] )
+	
+	field_lengths = {}
 end
